150ee25fba1ee7ac022e6597f4a48870bd04e83e
denise <denise@thecodewitches.de>
2026-06-03 16:54:32 +0200

    mini wording change DE

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -171,7 +171,7 @@ const translations = {
     "services.p1":
       "Wir bieten Software Entwicklung und technische Beratung aus einer Hand.",
     "services.p2":
-      "KI gehört in jede Werkzeugkiste. Wir wissen wann KI in die Lösung gehört und auch wann nicht.",
+      "KI gehört mittlerweile in jede Werkzeugkiste. Wir wissen, wann KI Teil der Lösung ist und auch, wann nicht.",
     "services.p3":
       "Wir tragen die Komplexität, damit ihr handlungsfähig bleibt. Jede Frage bringt uns weiter. Je unklarer das Problem, desto mehr zieht es uns an. Und wir bringen ansteckende Energie mit, versprochen.",
     "services.li1":
